c932f7f3129431f42fce94b59dcb28394fbfc13b
karlaclavel <karla.clavel1@gmail.com>
2024-05-18 21:47:55 -0600

    Se añadio menu Overflow y tabRow

--- app/src/main/java/uam/practica/menusjpc/BasicScaffold.kt
+++ app/src/main/java/uam/practica/menusjpc/BasicScaffold.kt
@@ -2,33 +2,25 @@ package uam.practica.menusjpc
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import uam.practica.menusjpc.ui.theme.MenusjpTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
---
+++ app/src/main/java/uam/practica/menusjpc/MenusTop.kt
@@ -0,0 +1,202 @@
+package uam.practica.menusjpc
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/*
+*  TopAppBar con menu de tipo OverFlow
+ */
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+//@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun OverFlowTopAppBar() {
+
+    var showMenu by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(0xFFABE0C3),
+            titleContentColor = Color(0xFF2C333C)
+        ),
+        title = { Text(text = "Overflow") },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Default.Favorite , contentDescription = "Icono")
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Default.Notifications , contentDescription = "Icono")
+            }
+            IconButton(onClick = { showMenu = !showMenu }) {
+                Icon(Icons.Default.MoreVert , contentDescription = "Icono")
+            }
+            DropdownMenu(
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false },
+                modifier = Modifier
+                    .background(Color(0xFFF3E8DB))
+                    .padding(10.dp)
+            ) {
+                DropdownMenuItem(
+                    leadingIcon = { Icon(Icons.Filled.AccountBox, contentDescription = null) },
+                    text = { Text(text="Cuenta")  },
+                    onClick = { showMenu = !showMenu }
+                )
+                DropdownMenuItem(
+                    leadingIcon = { Icon(Icons.Filled.Settings, contentDescription = null) },
+                    text = { Text(text="Configuración")  },
+                    onClick = { showMenu = !showMenu }
+                )
+
+            }
+        }
+
+    )
+
+}
+
+/*
+*  TabRow con tabs de movimiento
+ */
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TabRowTopBar() {
+    var tabIndex  by remember { mutableIntStateOf(0) }
+    val tabs = listOf ("Home", "Cuenta", "Configuracion")
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            modifier = Modifier,
+            contentColor = Color(0xFF2B254B),
+            containerColor = Color(0xFFBDACEC),
+            indicator = { tabPositions ->
+                TabRowDefaults.apply {
+                    Divider(
+                        Modifier
+                            .height(2.dp)
+                            .padding(horizontal = 10.dp)
+                            .tabIndicatorOffset(tabPositions[tabIndex]),
+                        color = Color(0xFF801392)
+                    )
+                }
+            },
+            divider = {}
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = tabIndex == index,
+                    onClick = { tabIndex = index },
+                    content = {
+                        when (index) {
+                            0 ->
+                                Column (
+                                    modifier = Modifier.padding(7.dp),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Icon(imageVector = Icons.Default.Home, contentDescription = null)
+                                    Text(text = "Home")
+                                }
+                            1 ->
+                                Column (
+                                    modifier = Modifier.padding(7.dp),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Icon(imageVector = Icons.Default.Person, contentDescription = null)
+                                    Text(text = "Cuenta")
+                                }
+                            2 ->
+                            Column (
+                                modifier = Modifier.padding(7.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+                                Text(text = "Configuración")
+                            }
+                        }
+                    }
+                )
+            }
+        }
+
+        when (tabIndex) {
+            0 -> HomeScreen()
+            1 -> CuentaScreen()
+            2 -> ConfiguracionScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Column(
+        Modifier.fillMaxSize().background(Color(0xFFE0B0C7)).padding(20.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home", fontSize = 25.sp)
+    }
+}
+
+@Composable
+fun CuentaScreen() {
+    Column(
+        Modifier.fillMaxSize().background(Color(0xFFB7D39C)).padding(20.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Cuenta", fontSize = 25.sp)
+    }
+}
+
+@Composable
+fun ConfiguracionScreen() {
+    Column(
+        Modifier.fillMaxSize().background(Color(0xFFB2CBD8)).padding(20.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Configuración", fontSize = 25.sp)
+    }
+}
